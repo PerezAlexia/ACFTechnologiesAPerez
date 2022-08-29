8b91e22788f205d284518ed75d4f116071faf887
lalen <lalen@LAPTOP-LVA0GV5M>
2022-08-28 22:08:01 -0500

    Agregar archivos de proyecto.

---
+++ GameHotterColder/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHotterColder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Parar = 0;
+            int NumeroIngreasado, contador = 0;
+
+            Random generar = new Random();
+
+            int NumeroRandom = generar.Next(1, 100);
+            
+            Console.WriteLine("El numero ha sido escogido");
+
+            do
+            {
+                Console.Write("Inserta un numero: ");
+                NumeroIngreasado = int.Parse(Console.ReadLine());
+
+                contador++;
+
+                if (NumeroRandom == NumeroIngreasado)
+                {
+                    Console.WriteLine("¡Lo tienes!.");
+                    Console.WriteLine("Vueltas: " + contador);
+
+                    contador = 0;
+
+                    
+                }
+
+                if (NumeroIngreasado > NumeroRandom)
+                {
+                    Console.WriteLine("Frio");
+                }
+
+                if (NumeroIngreasado < NumeroRandom)
+                {
+                    Console.WriteLine("Caliente");
+                }
+            }
+            while (NumeroIngreasado != Parar);
+
+
+
+
+        }
+    }
+}
---
+++ PL/Scripts/Consumir/Consumir.js
@@ -0,0 +1,66 @@
+﻿
+$(document).ready(function () {
+    GetAll();
+    
+});
+
+
+function GetAll() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/photos',
+        success: function (result) {//200 OK
+           
+            for (let i = 0; i < result.length; i++) {
+                for (let j = 0; j < result.length; j++) {
+                    var objeto = result[i, j];
+
+                    var filas =
+                        '<tr>'
+
+                        + "<td class='hidden'>" + objeto.albumId + "</td>"
+                        + "<td class='hidden'>" + objeto.id + "</td>"
+                        + "<td class='text-center'>" + objeto.title + "</td>"
+                        + "<td class='text-center'>" + objeto.url + "</ td>"
+                        + "<td class='text-center'>" + "<img src= " + objeto.thumbnailUrl + ">" + "</td>"
+
+                        + "</tr>";
+
+                    $("#tblConsumir tbody").append(filas);
+
+                    $('#tblConsumir').DataTable();
+
+                }
+            }
+
+        },
+        error: function (result) {
+            alert('Error en la consulta.' + result.responseJSON.ErrorMessage);
+
+        }
+    });
+}
+
+//function GetAll() {
+//    $.ajax({
+//        type: 'GET',
+//        url: 'https://randomuser.me/api/',
+//        success: function (result) {//200 OK 
+//            $('#tblUsuario tbody');
+
+//            var filas =
+//                '<tr>'
+//                + "<td class='text-center'>" + result.results[0].gender + "</td>"
+//                + "<td class='text-center'>" + result.results[0].name.title + " " + result.results[0].name.first + " " + result.results[0].name.last + "</td>"
+//                + "<td class='text-center'>" + result.results[0].location.street.number + result.results[0].location.street.name + "</td>"
+//                + "<td class='text-center'>" + result.results[0].email + "</td>"
+//                + "<td>" + "<img src= " + result.results[0].picture.large + ">" + "</td>"
+//                + "</tr>";
+//            $("#tblUsuario tbody").append(filas);
+
+//        },
+//        error: function (result) {
+//            alert('Error en la consulta.' + result.responseJSON.ErrorMessage);
+//        }
+//    });
+//}
